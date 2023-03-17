b773a3931a3aa52b82d68d479cf6c12dc0cd3509
fariszaky <114461567+fariszaky@users.noreply.github.com>
2023-03-17 15:55:27 +0700

    Add files via upload

---
+++ praktikum2/kondisional_ifelse.php
@@ -0,0 +1,10 @@
+<?php
+$nasi = "dibakar";
+
+if($nasi == "di goreng"){
+    echo "ya, ini di goreng";
+} elseif($nasi == "dibakar"){
+    echo "ya, ini dibakar";
+} else {
+    echo 'ini cuma nasi biasa';
+}
---
+++ praktikum2/kondisional_switchcase.php
@@ -0,0 +1,16 @@
+<?php
+$angka = 12;
+
+switch($angka){
+    case 9 :
+        echo "benar ini 9 !";
+        break;
+    case 8 :
+        echo "benar ini 8 !";
+    case 10 :
+        echo "ya, ini 10 !";
+        break;
+    default :
+        echo "ya ini berapa aja bisa";
+        break;
+}
